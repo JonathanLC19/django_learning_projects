395420a17899ba91da000f2716d9d6b8846619d6
Jonathan Lopez <jlopezcastellano19@gmail.com>
2022-12-29 10:18:02 +0000

    añadidas nuevas variables en views y templates projects y single project para practicar con variables y condicionales en páginas

--- CursoGithub/django/django_project_learning/projects/views.py
+++ CursoGithub/django/django_project_learning/projects/views.py
@@ -3,8 +3,44 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
+"""Trabajar un for loop en template con una lista de diccionarios añadiendo la lista a 'context' dentro de la vista """
+"""con key 'projects' y values como el nombre de la lista (línea 33)"""
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 def projects(request):
-    return(HttpResponse('These are your projects'))
+    """Pasar variables desde views que renderizar en templates"""
+    msg = "Hola qué disse"
+
+    """Pasar conjunto de variables para ejecutar funciones lógicas en templates (If, for,...)"""
+    number = 10
+    context = {'message': msg, 'number': number, 'projects': projectsList}
+
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return(HttpResponse('This is the info of the project' + ' ' + pk))
+    """renderizando un proyecto en el template basado en el key 'id', que pasaremos con el parámetro 'pk' de la vista"""
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+
+    context = {'project': projectObj}
+
+    return render(request, 'projects/single-project.html', context)
