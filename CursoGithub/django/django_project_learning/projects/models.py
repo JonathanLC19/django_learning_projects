47351d8655af584742af131f72ac42a132f87e14
Jonathan Lopez <jlopezcastellano19@gmail.com>
2022-12-29 13:21:58 +0000

    añadidos models reviews y tags, relaciones y añadidas querysets en views para traer atributos en templates a través de búsquedas en módulos

--- CursoGithub/django/django_project_learning/projects/models.py
+++ CursoGithub/django/django_project_learning/projects/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+class Projects(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null= True, blank= True)
+    demo_link = models.CharField(max_length=2000, null= True, blank= True)
+    source_link = models.CharField(max_length=2000, null= True, blank= True)
+
+    # crear un atributo para relación 'Many to Many' con el objeto Tag
+    tags = models.ManyToManyField('Tag', blank=True)
+
+    # crear dos atributos que cuenten los votos y el ratio de positivos y negativos
+    vote_total = models.IntegerField(default=0, null= True, blank= True)
+    vote_ratio = models.IntegerField(default=0, null= True, blank= True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default= uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    """creamos una tupla de tuplas 'VOTE_TYPE' que será las opciones de elección para el atributo 'value' añadidas con parámetro 'choice'. """
+    """el 1er valor de la tupla será el valor y el 2º el string que se muestra"""
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+
+    # owner =
+    project_fk = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    body = models.TextField(null= True, blank= True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default= uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default= uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
--- CursoGithub/django/django_project_learning/projects/views.py
+++ CursoGithub/django/django_project_learning/projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Projects
 
 # Create your views here.
 
@@ -25,21 +26,34 @@ projectsList = [
 ]
 
 def projects(request):
-    """Pasar variables desde views que renderizar en templates"""
-    msg = "Hola qué disse"
+    # """Pasar variables desde views que renderizar en templates"""
+    # msg = "Hola qué disse"
 
-    """Pasar conjunto de variables para ejecutar funciones lógicas en templates (If, for,...)"""
-    number = 10
-    context = {'message': msg, 'number': number, 'projects': projectsList}
+    # """Pasar conjunto de variables para ejecutar funciones lógicas en templates (If, for,...)"""
+    # number = 10
+    # context = {'message': msg, 'number': number, 'projects': projectsList}
+
+    # ------------------------------------------------------------------------------------------------------
+
+    """Pasar valores a templates con Queries en módulos. Vídeo 'Database Queries' """
+    projects = Projects.objects.all()
+
+    context = {'projects': projects}
 
     return render(request, 'projects/projects.html', context)
 
+
 def project(request, pk):
-    """renderizando un proyecto en el template basado en el key 'id', que pasaremos con el parámetro 'pk' de la vista"""
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    # """renderizando un proyecto en el template basado en el key 'id', que pasaremos con el parámetro 'pk' de la vista"""
+    # projectObj = None
+    # for i in projectsList:
+    #     if i['id'] == pk:
+    #         projectObj = i
+
+    # ------------------------------------------------------------------------------------------------------ 
+
+    """renderizar proyecto en template haciendo una query que nos dé el 'id' del proyecto, que se pasará con el parámetro 'pk'. Vídeo 'Database Queries' """
+    projectObj = Projects.objects.get(id=pk)
 
     context = {'project': projectObj}
 
