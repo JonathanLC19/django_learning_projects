917dafd22de93919338db4fee9ff0fb448600ec6
Jonathan Lopez <jlopezcastellano19@gmail.com>
2023-01-07 16:22:57 +0000

    fitrar skills con/sin descrip y arreglar fallo en link a owner en proyectos

--- CursoGithub/django/django_project_learning/users/views.py
+++ CursoGithub/django/django_project_learning/users/views.py
@@ -13,7 +13,14 @@ def profiles(request):
 
 def userProfile(request, pk):
     user_profile = Profile.objects.get(id=pk)
-    context = {'profile': user_profile}
+
+    # los modulos Profile y Skills están relacionados con la fk. Con 'skills_set' traemos el 
+    # conjunto de skills relacionadas con el 'user_profile' seleccionado y las filtra, primero
+    # por las que tienen descripcion (las excluye) y, segundo, por las que no (las incluye)
+    top_skills = user_profile.skills_set.exclude(description__exact="")
+    other_skills = user_profile.skills_set.filter(description="")
+
+    context = {'profile': user_profile,'topSkills': top_skills, 'otherSkills': other_skills}
 
     return render(request, 'users/user_profile.html', context)
     
